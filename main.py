6eb2b668fd41d478a7d170bce91ab1d7e7aa1e64
Daniel <37455228+danieljcoh@users.noreply.github.com>
2023-02-02 14:39:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,144 @@
+#a dungeon where the further you get the more of the truth you learn, but you can't learn the whole truth by simply completing it
+# what is the truth? 
+
+# ABOUT :: PROJECT LUNR MOONR -- The Lunar Effect -- Tellus Mater or Terra Mater ("Mother Earth")
+# you are a scientist awoken in a bunker by the last surviving crew member of the Terra-Moonr team. 
+# he explains that the moon is crashing into the earth and you are part of a team to help discover how to stop it
+# the bunker built underground was shut off from the surface due to the 
+# the moon coming closer is affecting the water content in people's minds and causing them to go crazy and experience moons of extreme violence
+
+
+
+
+#TEST MONSTER CLASS
+# class Monster:
+#   def __init__(self, name, health, attack_name, strength, sound, gold_value):
+#     self.name = name
+#     self.health = health
+#     self.attack_name = attack_name
+#     self.strength = strength
+#     self.sound = sound
+#     self.gold_value = gold_value
+
+# m1 = Monster("Gulago", 22, "Fierce Swipe", 2, "rggogorooww", 5)
+
+# print(m1.name)
+# print(m1.health)
+
+########################################
+
+#IMPORTS
+import time
+
+
+
+
+# STATS
+# player_health = 100;
+# player_name =
+
+
+
+# #INTRODUCTION
+print("Welcome to The Dungeon")
+print(r"""/_________    ___  ___      _______           ___           ___  ___      ________       ________      ________          _______       ________  ________  _______       ________      _________   
+|\___   ___\ |\  \|\  \    |\  ___ \         |\  \         |\  \|\  \    |\   ___  \    |\   __  \    |\   __  \        |\  ___ \     |\  _____\|\  _____\|\  ___ \     |\   ____\    |\___   ___\ 
+\|___ \  \_| \ \  \\\  \   \ \   __/|        \ \  \        \ \  \\\  \   \ \  \\ \  \   \ \  \|\  \   \ \  \|\  \       \ \   __/|    \ \  \__/ \ \  \__/ \ \   __/|    \ \  \___|    \|___ \  \_| 
+     \ \  \   \ \   __  \   \ \  \_|/__       \ \  \        \ \  \\\  \   \ \  \\ \  \   \ \   __  \   \ \   _  _\       \ \  \_|/__   \ \   __\ \ \   __\ \ \  \_|/__   \ \  \            \ \  \  
+      \ \  \   \ \  \ \  \   \ \  \_|\ \       \ \  \____    \ \  \\\  \   \ \  \\ \  \   \ \  \ \  \   \ \  \\  \|       \ \  \_|\ \   \ \  \_|  \ \  \_|  \ \  \_|\ \   \ \  \____        \ \  \ 
+       \ \__\   \ \__\ \__\   \ \_______\       \ \_______\   \ \_______\   \ \__\\ \__\   \ \__\ \__\   \ \__\\ _\        \ \_______\   \ \__\    \ \__\    \ \_______\   \ \_______\       \ \__\
+        \|__|    \|__|\|__|    \|_______|        \|_______|    \|_______|    \|__| \|__|    \|__|\|__|    \|__|\|__|        \|_______|    \|__|     \|__|     \|_______|    \|_______|        \|__|
+                                                                                                                                                                                                   """)
+print(r"""/         ___---___                    
+      .--         --.      
+    ./   ()      .-. \.
+   /   o    .   (   )  \
+  / .            '-'    \         
+ | ()    .  O         .  |      
+|                         |      
+|    o           ()       |
+|       .--.          O   |            
+ | .   |    |            |
+  \    `.__.'    o   .  /    
+   \                   /                   
+    `\  o    ()      /'         
+      `--___   ___--'
+            ---""")
+
+print()
+print()
+print()
+
+
+print("Hey...")
+time.sleep(1)
+print("Hey... Wake up!")
+
+
+print()
+print()
+print()
+
+print(r"""/                                                   __________________
+        ||         |||||||||||||||||||             /                  \
+    ____||         |||||||||||||||||||            /                    \
+    \\\\  []       |||||||||||||||||||           /                      \
+     \____/        |                 |          /                       |
+     |_____|      _|__ __       __ __|_        /                        |
+     |     |     ( |  (_o)-/~\-(o_)  | )      /                         |
+     |     |      (|      (   )      |)      /\                         |
+     |     |       |                 |      /  \                        |
+     |     |_______|    /       \    |_________ \                       |
+     |     |        \     _____     /          \ \                      |
+     |     |         \   (_____)   /            \ \                     |
+     |     |          \___________/              | \ __________________/
+     |     |    |      |||||||||||               |
+     |     /   /        |||||||||       |        |
+     |        /          |||||||        |        |
+     |       /              {o          |        |
+      \_____/               {o          |        |
+          |                 {o          |        |
+          |                 {o          |        |""")
+
+print()
+print()
+print()
+
+print("Oh thank goodness you're awake! The serum worked.")
+
+# PLAYER STATS
+player_name = input(print("Now tell me: Do you remember your name? "))
+player_health = 100;
+# PLAYER_INVENTORY -- You cannot add more space you can only delete other things.
+player_inventory = ["Empty", "Empty", "Empty", "Empty", "Empty"]
+
+print(player_name + " " + "hmmm. That's not the name I remember but I'll go with it for now. Your brain is probably a little fuzzy still from when you hit your head.")
+
+print("Well... " + player_name + ". " + "We don't have very long. I'll just tell you it all again since I doubt you remember why you're here.")
+
+# I REALLY LIKE THE IDEA OF THE PLAYER BEING TOLD WHY THEY ARE HERE AND THEN THEM JUST ACCEPTING IT, BUT WHAT IF THAT'S THE PROBLEM?
+
+print("")
+
+
+# health = 100
+# answer = input("Please pick your power: telekenesis(t), explosion(e), or gravity(g)")
+
+# if answer == "t":
+#   print("You've chosen Telekenesis. You have a 100 health. Now go and discover the secrets.")
+# elif answer == "e":
+#   print("You've chosen Explosion. You have a 100 health. Now go and discover the secrets.")
+# else:
+#   print("You've chosen Gravity. You have a 100 health. Now go and discover the secrets.")
+
+# road_fork = print("You arrive at a fork in the road. Do you want to go left or right?")
+
+# if road_fork == "right":
+#   print("You turn right and a monster appears. It attacks you.")
+#   print("A gulago has appeared.")
+#   print(m1.sound)
+  
+  
+#while health > 0:
+  #print(f"You have {health} health. Keep going!")
+
